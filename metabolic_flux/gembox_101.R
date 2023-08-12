da420fbbd6c92abe0d5ada07cd5bc3ec89f9d6c2
ming <ming@mings-MacBook-Pro.local>
2023-08-11 20:24:33 -0700

    new code

--- metabolic_flux/gembox_101.R
+++ metabolic_flux/gembox_101.R
@@ -4,5 +4,20 @@
 # check out install_gurobi.R
 #devtools::install_github("ruppinlab/gembox")
 library(gembox)
+## lee et al. BMC Syst Biol 2012: https://github.com/ruppinlab/gembox/blob/master/R/exprs.R
 
-# lee et al. BMC Syst Biol 2012: https://github.com/ruppinlab/gembox/blob/master/R/exprs.R
+#README.md from https://github.com/ruppinlab/covid_metabolism
+# 1, download data
+#$ cd covid_metabolism-main/data
+#$pip3 install gdown #for use gdown in ./dnload.sh
+#$ ./dnload.sh
+# covid_metabolism_files.tar.gz (270MB), liao.RDS(3.6G), chua.RDS(1.7G) files.
+
+# process data `collect.validation.data.R`
+#BiocManager::install('GSA')    
+#devtools::install_github("ImNotaGit/my.utils")
+#library(my.utils)
+(files=Sys.glob('../my.utils_R/*R')) #download `ImNotaGit/my.utils` from github
+# comment out `rmote::xxx` then source files
+#install.packages("httpgd")
+lapply(files, source)
