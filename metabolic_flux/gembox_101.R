10a120f4515d1149cd549c7a941fa934cc362a5c
Ming Yang <mingyang@Mings-iMac-2.local>
2023-08-15 16:33:20 -0700

    new code

--- metabolic_flux/gembox_101.R
+++ metabolic_flux/gembox_101.R
@@ -17,7 +17,19 @@ library(gembox)
 #BiocManager::install('GSA')    
 #devtools::install_github("ImNotaGit/my.utils")
 #library(my.utils)
-(files=Sys.glob('../my.utils_R/*R')) #download `ImNotaGit/my.utils` from github
-# comment out `rmote::xxx` then source files
+(files=Sys.glob('../my.utils/R/*R')) #download `ImNotaGit/my.utils` from github
+# https://github.com/ImNotaGit/my.utils
+# comment out `rmote::xxx` in `utils.R`, install 'httpgd', then source files.
 #install.packages("httpgd")
 lapply(files, source)
+
+# an example from Genome-scale metabolic modeling reveals SARS-CoV-2-induced metabolic changes and antiviral targets
+# https://www.embopress.org/doi/full/10.15252/msb.202110260
+# full code repo: https://github.com/ruppinlab/covid_metabolism
+# download required data on google drive 
+# `Alternatively, one can manually download the data files from this Google Drive link then decompress them into the data folder.`
+# you'd have `covid_metabolism_files.tar.gz`.
+# there is `de.and.gsea.res.RData` in folder `expression`.
+# go to `covid_metabolism/GEM` folder, run `prepare.data.R` and `imat.and.mta.R` scripts.
+
+
